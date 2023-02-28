832197d062d443cf25bf05a5ad72dbffc0f4b8fd
Otmane Aatik <aaotmane@gmail.com>
2023-02-28 09:42:24 -0500

    data file(structured manually)

---
+++ data.js
@@ -0,0 +1,302 @@
+export const data = [
+  {
+    img: "argentina.png",
+    options: ["guatemala", "argentina", "chile", "uruguay"],
+    correctAnswer: "argentina",
+  },
+  {
+    img: "brazil.png",
+    options: ["bolivia", "syria", "egypt", "brazil"],
+    correctAnswer: "brazil",
+  },
+  {
+    img: "canada.png",
+    options: ["canada", "lebanon", "iraq", "tunisia"],
+    correctAnswer: "canada",
+  },
+  {
+    img: "egypt.png",
+    options: ["syria", "egypt", "ireland", "iraq"],
+    correctAnswer: "egypt",
+  },
+  {
+    img: "germany.png",
+    options: ["belgium", "ireland", "jordan", "germany"],
+    correctAnswer: "germany",
+  },
+  {
+    img: "iraq.png",
+    options: ["iraq", "syria", "morocco", "yemen"],
+    correctAnswer: "iraq",
+  },
+  {
+    img: "lebanon.png",
+    options: ["canada", "lebanon", "iran", "morocco"],
+    correctAnswer: "lebanon",
+  },
+  {
+    img: "mexico.png",
+    options: ["italy", "iran", "mexico", "albania"],
+    correctAnswer: "mexico",
+  },
+  {
+    img: "somalia.png",
+    options: ["vietnam", "somalia", "tajikistan", "pakistan"],
+    correctAnswer: "somalia",
+  },
+  {
+    img: "uzbekistan.png",
+    options: ["uzbekistan", "tajikistan", "kyrgyzstan", "hungary"],
+    correctAnswer: "uzbekistan",
+  },
+  {
+    img: "india.png",
+    options: ["niger", "india", "pakistan", "mexico"],
+    correctAnswer: "india",
+  },
+  {
+    img: "sudan.png",
+    options: ["palestine", "united arab emirates", "sudan", "kuwait"],
+    correctAnswer: "sudan",
+  },
+  {
+    img: "saudi-arabia.png",
+    options: ["egypt", "saudi arabia", "Haiti", "qatar"],
+    correctAnswer: "saudi arabia",
+  },
+  {
+    img: "hongkong.png",
+    options: ["hong kong", "Haiti", "china", "Timor-Leste"],
+    correctAnswer: "hong kong",
+  },
+  {
+    img: "laos.png",
+    options: ["Togo", "peru", "nicaragua", "laos"],
+    correctAnswer: "laos",
+  },
+  {
+    img: "nicaragua.png",
+    options: ["nicaragua", "Vanuatu", "Turkmenistan", "maldives"],
+    correctAnswer: "nicaragua",
+  },
+  {
+    img: "maldives.png",
+    options: ["Zambia", "maldives", "Vanuatu", "turkey"],
+    correctAnswer: "maldives",
+  },
+  {
+    img: "nauru.png",
+    options: ["spain", "Vanuatu", "oman", "nauru"],
+    correctAnswer: "nauru",
+  },
+  {
+    img: "monaco.png",
+    options: ["monaco", "maldives", "france", "Zimbabwe"],
+    correctAnswer: "monaco",
+  },
+  {
+    img: "luxembourg.png",
+    options: ["russia", "luxembourg", "bahrain", "netherlands"],
+    correctAnswer: "luxembourg",
+  },
+  {
+    img: "qatar.png",
+    options: ["bahrain", "qatar", "Madagascar", "lebanon"],
+    correctAnswer: "qatar",
+  },
+  {
+    img: "libya.png",
+    options: ["pakistan", "niger", "iraq", "libya"],
+    correctAnswer: "libya",
+  },
+  {
+    img: "sweden.png",
+    options: ["sweden", "denmark", "mali", "norway"],
+    correctAnswer: "sweden",
+  },
+  {
+    img: "chad.png",
+    options: ["romania", "togo", "guinea", "chad"],
+    correctAnswer: "chad",
+  },
+  {
+    img: "mali.png",
+    options: ["guinea", "mali", "lithuania", "senegal"],
+    correctAnswer: "mali",
+  },
+  {
+    img: "bolivia.png",
+    options: ["bolivia", "chile", "Madagascar", "Benin"],
+    correctAnswer: "bolivia",
+  },
+  {
+    img: "nepal.png",
+    options: ["peru", "nepal", "tunisia", "Benin"],
+    correctAnswer: "nepal",
+  },
+  {
+    img: "turkey.png",
+    options: ["pakistan", "morocco", "tunisia", "turkey"],
+    correctAnswer: "turkey",
+  },
+  {
+    img: "thailand.png",
+    options: ["thailand", "russia", "zimbabwe", "luxembourg"],
+    correctAnswer: "thailand",
+  },
+  {
+    img: "zimbabwe.png",
+    options: ["ethiopia", "zimbabwe", "iran", "mozambique"],
+    correctAnswer: "zimbabwe",
+  },
+  {
+    img: "tajikistan.png",
+    options: ["tajikistan", "oman", "kenya", "hungary"],
+    correctAnswer: "tajikistan",
+  },
+  {
+    img: "mongolia.png",
+    options: ["liechtenstein", "somalia", "chad", "mongolia"],
+    correctAnswer: "mongolia",
+  },
+  {
+    img: "moldova.png",
+    options: ["moldova", "haiti", "uruguay", "andorra"],
+    correctAnswer: "moldova",
+  },
+  {
+    img: "uruguay.png",
+    options: ["peru", "uruguay", "rwanda", "paraguay"],
+    correctAnswer: "uruguay",
+  },
+  {
+    img: "sierra-leone.png",
+    options: ["sierra leone", "rwanda", "ecuador", "uzbekistan"],
+    correctAnswer: "sierra leone",
+  },
+  {
+    img: "colombia.png",
+    options: ["ecuador", "venezuela", "mexico", "colombia"],
+    correctAnswer: "colombia",
+  },
+  {
+    img: "sri-lanka.png",
+    options: ["tajikistan", "pakistan", "sri lanka", "kazakhstan"],
+    correctAnswer: "sri lanka",
+  },
+  {
+    img: "kyrgyzstan.png",
+    options: ["kyrgyzstan", "tajikistan", "kazakhstan", "indonesia"],
+    correctAnswer: "kyrgyzstan",
+  },
+  {
+    img: "georgia.png",
+    options: ["england", "indonesia", "australia", "georgia"],
+    correctAnswer: "georgia",
+  },
+  {
+    img: "tanzania.png",
+    options: ["zimbabwe", "tanzania", "taiwan", "mozambique"],
+    correctAnswer: "tanzania",
+  },
+  {
+    img: "china.png",
+    options: ["vietnam", "taiwan", "nigeria", "china"],
+    correctAnswer: "china",
+  },
+  {
+    img: "nigeria.png",
+    options: ["cameroon", "nigeria", "ghana", "mali"],
+    correctAnswer: "nigeria",
+  },
+  {
+    img: "cameroon.png",
+    options: ["cameroon", "senegal", "hungary", "guinea"],
+    correctAnswer: "cameroon",
+  },
+  {
+    img: "niger.png",
+    options: ["india", "hungary", "kenya", "niger"],
+    correctAnswer: "niger",
+  },
+  {
+    img: "south-sudan.png",
+    options: ["south sudan", "south africa", "chile", "palestine"],
+    correctAnswer: "south sudan",
+  },
+  {
+    img: "chile.png",
+    options: ["puerto rico", "chile", "russia", "cuba"],
+    correctAnswer: "chile",
+  },
+  {
+    img: "netherlands.png",
+    options: ["luxembourg", "netherlands", "colombia", "italy"],
+    correctAnswer: "netherlands",
+  },
+  {
+    img: "ecuador.png",
+    options: ["venezuela", "mexico", "guinea", "ecuador"],
+    correctAnswer: "ecuador",
+  },
+  {
+    img: "rwanda.png",
+    options: ["rwanda", "ghana", "chad", "cameroon"],
+    correctAnswer: "rwanda",
+  },
+  {
+    img: "cambodia.png",
+    options: ["china", "nepal", "burundi", "cambodia"],
+    correctAnswer: "cambodia",
+  },
+  {
+    img: "burundi.png",
+    options: ["burundi", "libya", "eritrea", "south africa"],
+    correctAnswer: "burundi",
+  },
+  {
+    img: "eritrea.png",
+    options: ["eritrea", "ethiopia", "morocco", "kenya"],
+    correctAnswer: "eritrea",
+  },
+  {
+    img: "turkmenistan.png",
+    options: ["tajikistan", "turkmenistan", "austria", "uzbekistan"],
+    correctAnswer: "turkmenistan",
+  },
+  {
+    img: "latvia.png",
+    options: ["austria", "bolivia", "jordan", "latvia"],
+    correctAnswer: "latvia",
+  },
+  {
+    img: "jordan.png",
+    options: ["jordan", "palestine", "syria", "sudan"],
+    correctAnswer: "jordan",
+  },
+  {
+    img: "austria.png",
+    options: ["lithuania", "latvia", "serbia", "austria"],
+    correctAnswer: "austria",
+  },
+  {
+    img: "serbia.png",
+    options: ["slovakia", "serbia", "zimbabwe", "slovenia"],
+    correctAnswer: "serbia",
+  },
+  {
+    img: "zambia.png",
+    options: ["zambia", "zimbabwe", "jamaica", "moldova"],
+    correctAnswer: "zambia",
+  },
+  {
+    img: "malawi.png",
+    options: ["malawi", "mali", "slovenia", "serbia"],
+    correctAnswer: "malawi",
+  },
+  {
+    img: "swaziland.png",
+    options: ["slovenia", "sweden", "swaziland", "spain"],
+    correctAnswer: "swaziland",
+  },
+];
--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-console.log("Hellooooo!")
+import { data } from './data.js'
+console.log(data)
