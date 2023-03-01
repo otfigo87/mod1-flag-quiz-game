ab5c20cb0c49aaabf6b8ef73543446cb897d36a1
Otmane Aatik <aaotmane@gmail.com>
2023-03-01 16:02:17 -0500

    Day2 show results

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 import { data } from './data.js';
 // console.log(data)
 //! Select my elements
+let flagImgDiv = document.querySelector(".flag-img");
 let flagOptions = document.querySelector(".flag-options ul");
 let flagImg = document.querySelector(".flag-img img");
 let flagList = document.querySelectorAll(".flag-options ul li");
 let score = document.querySelector("h3>span");
+let scoreDiv = document.querySelector(".score");
+let correctAns = document.querySelector(".score .correct span");
+let incorrectAns = document.querySelector(".score .incorrect span");
+let btnNewGame = document.querySelector("#newGame");
 let countSpan = document.querySelector(".count>span");
+let countDiv = document.querySelector(".count");
+let scoreBox = document.querySelector(".box");
 let count = 10;
 countSpan.innerText = count;
 
 //! My variables
 let questions = data.sort(() => Math.random() - Math.random()).slice(0, 10);
 console.log(questions);// 10 random questions
-let allQuestions= 10;
 let currentQuestion = 0;
-let scoreNum = 0;
 let correctAnswers = 0;
-let wrongAnswer = 0;
 let answer = "";
 
 
@@ -27,11 +31,12 @@ const startGame = () => {
 
 //loop between questions
 const nextQuestion = (obj) => {
-    if(currentQuestion < allQuestions){
+    if(currentQuestion < 10){
         flagImg.src = `img/${obj.img}`;
 
         for (let i = 0; i < flagList.length; i++){ 
-            flagList[i].innerHTML = obj.options[i]        
+            flagList[i].innerHTML = obj.options[i];
+            flagList[i].classList.remove("selected", "success", "wrong");     
         }
         answer = obj.correctAnswer;
         console.log(answer);
@@ -48,9 +53,19 @@ flagList.forEach(li => {
     li.classList.remove("selected", "success", "wrong");
     li.addEventListener("click", () => {
         li.classList.add("selected")
-        checkAnswer(answer);
+
+        setTimeout(() => {
+          checkAnswer(answer);
+        }, 500);
          
-        nextQuestion(questions[currentQuestion]);
+        setTimeout(() => {
+            nextQuestion(questions[currentQuestion]);
+        }, 1000)
+
+        
+        showResult()
+     
+       
     });
 });
 
@@ -59,22 +74,42 @@ flagList.forEach(li => {
 //Check answer
 const checkAnswer = (rightAnswer) => {
     for( let i = 0; i < flagList.length; i++){
-        let chosenAnswer = "";
+        let chosenAnswer;
         if(flagList[i].classList.contains("selected")){
-            chosenAnswer = flagList[i];
-            if(chosenAnswer === rightAnswer ){
-                flagList[i].classList.add("success");
-                correctAnswers++
-                score.innerHTML = correctAnswers;
+            chosenAnswer = flagList[i].textContent;
+            console.log(chosenAnswer)
+            if (chosenAnswer === rightAnswer) {
+              flagList[i].classList.add("success");
+              correctAnswers++;
+              score.innerHTML = correctAnswers;
             } else {
-                flagList[i].classList.add("wrong")
+              flagList[i].classList.add("wrong");
             }
         }
     }
 }
 
+//show result
+const showResult = () => {
+    if(count === 0) { // Run only when you get to the last question
+        flagOptions.innerHTML = "";
+        flagImgDiv.innerHTML = "";
+        scoreDiv.style.display = "flex";
+        correctAns.innerHTML = correctAnswers;
+        incorrectAns.innerHTML = 10 - correctAnswers;
+        scoreBox.innerHTML = ""
+
+    } 
+
+}
+
 
 //Start The Game
 window.addEventListener("load", startGame);
+
+//To Generate A New Game
+btnNewGame.addEventListener('click', () => {
+    window.location.reload();
+});
 
 
